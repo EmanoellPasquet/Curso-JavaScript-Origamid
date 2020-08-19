10053e7dfa99a8018d3b04e7d0fb3ee169744ec6
Emanoell Pasquet <emanoell.pasquet@gmail.com>
2020-08-19 15:44:51 -0300

    Começando o curso do zero

---
+++ scripts/index.js
@@ -0,0 +1,9 @@
+let test1 = "olá";
+let test2 = "22";
+let idade = 29;
+let nome = "Emanoell";
+let sobrenome = "Pasquet";
+let nomeCompleto = nome + sobrenome;
+var frase = "It's time";
+
+console.log(test1, test2, idade, nome, sobrenome, nomeCompleto, typeof nome);
