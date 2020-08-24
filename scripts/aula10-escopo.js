f7de91fec21dc124fbc9ecd42917294d96c7715d
Emanoell Pasquet <emanoell.pasquet@gmail.com>
2020-08-24 18:16:15 -0300

    📦 NEW: aula 10 - Escopo

--- scripts/aula10-escopo.js
+++ scripts/aula10-escopo.js
@@ -1,29 +1,29 @@
 // Por qual motivo o código abaixo retorna com erros?
 {
-   var cor = 'preto';
-   const marca = 'Fiat';
-   let portas = 4;
- }
- console.log(var, marca, portas);
- 
- // Como corrigir o erro abaixo?
- function somarDois(x) {
-   const dois = 2;
-   return x + dois;
- }
- function dividirDois(x) {
-   return x + dois;
- }
- somarDois(4);
- dividirDois(6);
- 
- // O que fazer para total retornar 500?
- var numero = 50;
- 
- for(var numero = 0; numero < 10; numero++) {
-   console.log(numero);
- }
- 
- const total = 10 * numero;
- console.log(total);
- 
+  var cor = "preto";
+  const marca = "Fiat";
+  let portas = 4;
+}
+console.log(/*var,*/ cor, marca, portas); //está jogando um 'var' dentro do console log e também porque console está fora do escopo de const e let
+
+// Como corrigir o erro abaixo?
+function somarDois(x) {
+  const dois = 2; //deve ser declarado do lado de fora
+  return x + dois;
+}
+function dividirDois(x) {
+  return x + dois; //é pra dividir, não somar
+}
+somarDois(4);
+dividirDois(6);
+
+// O que fazer para total retornar 500?
+var numero = 50; // definir como const
+
+for (var numero = 0; numero < 10; numero++) {
+  //definir como let
+  console.log(numero);
+}
+
+const total = 10 * numero;
+console.log(total);
