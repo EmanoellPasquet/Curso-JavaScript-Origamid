eff076d4e187ffba6e6377e1d1e413eb7af0d9b2
Emanoell Pasquet <emanoell.pasquet@gmail.com>
2020-08-26 20:37:57 -0300

    👌 IMPROVE: métodos de seleção via JS

---
+++ projeto/script.js
@@ -0,0 +1,13 @@
+// alguns métodos de seleção de items do html via javascript
+
+const animais = document.getElementById("animais");
+
+const gridSection = document.getElementsByClassName("grid-section");
+
+const firstLi = document.querySelector("li");
+
+const firstLi = document.querySelector("ul");
+
+const linksInternos = document.querySelector('[href^="#"]');
+
+const animaisImg = document.querySelectorAll(".animais img");
