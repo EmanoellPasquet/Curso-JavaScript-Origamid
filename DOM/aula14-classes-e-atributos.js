376190dfcf459758d9d87f0140755b3a95d52fbe
Emanoell Pasquet <emanoell.pasquet@gmail.com>
2020-09-23 18:44:58 -0300

    📦 NEW: Aula 14

--- DOM/aula14-classes-e-atributos.js
+++ DOM/aula14-classes-e-atributos.js
@@ -1,7 +1,24 @@
 // Adicione a classe ativo a todos os itens do menu
+const menuItems = document.querySelectorAll('.menu a')
+menuItems.forEach((item) => {
+   item.classList.add('ativo')
+})
 
 // Remove a classe ativo de todos os itens do menu e mantenha apenas no primeiro
-
+menuItems.forEach((item) => {
+   item.classList.remove('ativo')
+})
+menuItems[0].classList.add('ativo')
 // Verifique se as imagens possuem o atributo alt
 
+const img = document.querySelectorAll('img')
+
+img.forEach((item) => {
+   // console.log(img, item.hasAttribute('alt'))
+})
+
 // Modifique o href do link externo no menu
+
+const link = document.querySelector('a[href^="http"]')
+link.setAttribute('href', 'newAttribute')
+console.log(link)
